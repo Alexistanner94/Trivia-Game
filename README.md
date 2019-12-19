4bfd88fadbe3d9b595d67bb80f3181f6b183fdfa
Alexis Tanner <alexistanner94@hotmail.com>
2019-12-18 22:29:14 -0500

    questions created

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Trivia-Game
+
+"Who lives in a pineapple under the sea?"
+
+If you were able finish this song then you should definitely try to beat this Bikini Bottom Trivia!
+
+Here is how it works :
+
+First, Youll want to make
---
+++ assets/javascript/questions.js
@@ -0,0 +1,84 @@
+let questions = [
+  {
+    question: "What is the name of the snail Gary falls in love with?",
+    answers: ["Penelope", "Jessica", "Cynthia", "Mary"],
+    correctAnswer: "Mary"
+  },
+  {
+    question: "What does Mermaid Main wear on his feet?",
+    answers: [
+      "Blue fuzzy slippers",
+      "Fuzzy pink slippers",
+      "Soccer cleats",
+      "Bananas"
+    ],
+    correctAnswer: "Fuzzy pink slippers"
+  },
+
+  {
+    question:
+      "When Spongebob and Patrick adopt a baby scallop,What did they name it?",
+    answers: [" Junior", "7", "Lil Fry", "Melvin"],
+    correctAnswer: "Junior"
+  },
+  {
+    question: "What is the name of Mr.Krabs' pet worm?",
+    answers: ["Krabby Worm", "Turd", "Whichsideisthehead WIlly", "Mr.Doodles"],
+    correctAnswer: "Mr.Doodles"
+  },
+  {
+    question: "Who are Spongebob's parents?",
+    answers: [
+      "Harold & Margaret Squarepants",
+      "Ren & Stimpy Squarepants",
+      "Charles & Judy Squarepants",
+      "Terry & Terri Squrepants"
+    ],
+    correctAnswer: "Harold & Margaret Squarepants"
+  },
+  {
+    question: "Who exercises on a hamster wheel?",
+    answers: ["Mrs.Puff", "Patrick", " Squidward", "Sandy"],
+    correctAnswer: "Sandy"
+  },
+  {
+    question: "When did Spongebob get his boating license?",
+    answers: [
+      "When he was born",
+      "Never",
+      "After Mrs.Puff gave up",
+      "On his 5th try"
+    ],
+    correctAnswer: "When he was born"
+  },
+  {
+    question: "WHo signs a spatula for Spongebob?",
+    answers: [
+      "Larry the Lobster",
+      "Taylor Swift",
+      "Barnacle Boy",
+      "Kenny the Cat"
+    ],
+    correctAnswer: "Kenny the Cat"
+  },
+  {
+    question: "What does Gary do on Opposite Day?",
+    answers: [
+      "Wash Squidward'feet",
+      "Eat olive nachos",
+      "Take off his shell",
+      "Bark"
+    ],
+    correctAnswer: "Bark"
+  },
+  {
+    question: "What color are Patrick's shorts?",
+    answers: [
+      "Green and Purple",
+      "Pink and Blue",
+      "Patrick doesn't wear pants",
+      "Yellow and Pink"
+    ],
+    correctAnswer: "Green and Purple"
+  }
+];
